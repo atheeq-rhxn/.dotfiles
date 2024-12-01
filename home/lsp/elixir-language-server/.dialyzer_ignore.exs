cea10ffaed34d2a1f0e86ff6819954f520bbc953
atheeq <atheeq.rhxn@gmail.com>
2024-12-01 11:41:22 +0530

    lsp changes

---
+++ home/lsp/elixir-language-server/.dialyzer_ignore.exs
@@ -0,0 +1,4 @@
+[
+  {"lib/launch.ex", :unknown_function},
+  {"lib/language_server/providers/execute_command/restart.ex", :no_return}
+]
---
+++ home/lsp/elixir-language-server/apps/language_server/lib/language_server/dialyzer/supervisor.ex
@@ -0,0 +1,19 @@
+defmodule ElixirLS.LanguageServer.Dialyzer.Supervisor do
+  use Supervisor
+
+  def start_link(parent \\ self(), name \\ nil, root_path, dialyzer_module) do
+    Supervisor.start_link(__MODULE__, {parent, root_path, dialyzer_module},
+      name: name || __MODULE__
+    )
+  end
+
+  @impl Supervisor
+  def init({parent, root_path, dialyzer_module}) do
+    Supervisor.init(
+      [
+        {dialyzer_module, {parent, root_path}}
+      ],
+      strategy: :one_for_one
+    )
+  end
+end
